662d7e92b53dd7021b20e59a3b8e2aed0f4ddf80
Dayvyson Almneida <dayvysonalmeida@gmail.com>
2024-05-04 19:43:27 -0300

    Importação do TodoUpdateView mais path com busca de tarefa expecifica

--- setup/urls.py
+++ setup/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import TodoListView, TodoCreateView
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView
 
 
 
@@ -26,4 +26,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", TodoListView.as_view(), name="todo_list"),
     path("create", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
 ]
