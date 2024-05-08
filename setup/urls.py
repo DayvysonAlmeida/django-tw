bfe81812bf039845709be01aa7466ba9df942f26
Dayvyson Almneida <dayvysonalmeida@gmail.com>
2024-05-08 01:48:15 -0300

    atualização delete view

--- setup/urls.py
+++ setup/urls.py
@@ -2,7 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import TodoListView, TodoCreateView, TodoUpdateView
+from todos.views import     TodoListView,     TodoCreateView,    TodoUpdateView, TodoDeleteView, View
+
 
 
 
@@ -11,4 +12,6 @@ urlpatterns = [
     path("", TodoListView.as_view(), name="todo_list"),
     path("create", TodoCreateView.as_view(), name="todo_create"),
     path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+    path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
+    # path("complete/<int:pk>", TodoCompleteView.as_view(), name="todo_complete"),
 ]
